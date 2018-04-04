fe941644b329f0083f928a736c3ff98fde26291c
ponytoj <daniel.villalobosdel@gmail.com>
2018-04-04 19:36:29 +0200

    Creada la clase cartaEnMano

--- hamate/src/com/MVDV/PL2/cartaEnMano.java
+++ hamate/src/com/MVDV/PL2/cartaEnMano.java
@@ -1,4 +1,34 @@
 package com.MVDV.PL2;
 
-public class cartaEnMano {
+import java.util.ArrayList;
+
+public class cartaEnMano extends  cartaEnMazo {
+    private boolean maquina; //esto ayuda a representar el color
+    private boolean enMano;
+
+    public cartaEnMano() {
+    }
+
+    public cartaEnMano(int valorIzq, int valorDer, boolean maquina) {
+        super(valorIzq, valorDer, false);
+        this.maquina = maquina;
+        this.enMano = true;
+    }
+
+    public boolean isMaquina() {
+        return maquina;
+    }
+
+    public void setMaquina(boolean maquina) {
+        this.maquina = maquina;
+    }
+
+    public boolean isEnMano() {
+        return enMano;
+    }
+
+    public void setEnMano(boolean enMano) {
+        this.enMano = enMano;
+    }
+
 }
