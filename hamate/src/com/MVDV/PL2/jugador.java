902d4b7ec304f1ada048fcc075f63f5786a1a34a
ponytoj <daniel.villalobosdel@gmail.com>
2018-04-04 19:37:40 +0200

    Creada la clase jugador y añadido el método que permite recibir cartas

--- hamate/src/com/MVDV/PL2/jugador.java
+++ hamate/src/com/MVDV/PL2/jugador.java
@@ -1,4 +1,32 @@
 package com.MVDV.PL2;
 
+import java.util.ArrayList;
+
 public class jugador {
+    private boolean maquina;
+    private ArrayList <cartaEnMano> mano = new ArrayList<>();
+
+    public jugador(boolean maquina) {
+        this.maquina = maquina;
+    }
+
+    public boolean isMaquina() {
+        return maquina;
+    }
+
+    public void setMaquina(boolean maquina) {
+        this.maquina = maquina;
+    }
+
+    public boolean isLlena(){
+       return (mano.size() == 5);
+    }
+
+    public void recibirCarta(cartaEnMazo cartaRobada){
+        if (!isLlena()) {
+            cartaEnMano cartaAux = new cartaEnMano(cartaRobada.getValorIzq(), cartaRobada.getValorDer(), this.isMaquina());
+            mano.add(cartaAux);
+        }
+
+    }
 }
