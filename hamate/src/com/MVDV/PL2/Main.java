788c13500da4af3c9fb7315c525d8b8b630f5b05
ponytoj <daniel.villalobosdel@gmail.com>
2018-04-04 19:37:58 +0200

    Primer acercamiento al main final

--- hamate/src/com/MVDV/PL2/Main.java
+++ hamate/src/com/MVDV/PL2/Main.java
@@ -7,5 +7,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String res = "SI";
+
+        //TODO generar el mazo
+
+        do{
+            //res=partida.jugarJuego();
+
+
+        }while (res.equals("NO"));
     }
 }
