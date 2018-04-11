a77b5f239e892c70f8a8d40424c1f60b75dbb09c
ponytoj <daniel.villalobosdel@gmail.com>
2018-04-11 12:36:19 +0200

    Subo lo que ya estaba hecho en el Main

--- src/com/MVDV/PL2/Main.java
+++ src/com/MVDV/PL2/Main.java
@@ -1,8 +1,21 @@
 package com.MVDV.PL2;
 
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner entrada = new Scanner(System.in);
+        String res = "SI";
+
+        //TODO generar el mazo
+
+        do{
+            //res=partida.jugarJuego();
+
+
+        }while (res.equals("NO"));
     }
 }
